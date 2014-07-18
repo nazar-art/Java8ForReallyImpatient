89a9e0ed03229a46ed3c9c864cf32b32ee2991a2
Nazar Lelyak <nazar.lelyak@gmail.com>
2014-07-18 18:01:11 +0300

    add code

---
+++ src/main/java/com/demo/ch1/FunctionalInterfaces.java
@@ -0,0 +1,52 @@
+package com.demo.ch1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+import java.util.function.BiFunction;
+
+public class FunctionalInterfaces extends Application {
+    @Override
+    public void start(Stage stage) {
+        String[] strings = "Mary had a little lamb".split(" ");
+
+        Arrays.sort(strings,
+                (first, second) -> Integer.compare(first.length(), second.length()));
+
+        System.out.println(Arrays.toString(strings));
+
+        Button button = new Button("Click me!");
+        button.setOnAction(
+                event -> System.out.println("Thanks for clicking!"));
+
+        stage.setScene(new Scene(button));
+        stage.show();
+
+        BiFunction<String, String, Integer> comp
+                = (first, second) -> Integer.compare(first.length(), second.length());
+        // Arrays.sort(strings, comp);
+        // Error: Arrays.sort doesn't want a BiFunction
+
+        // Runnable sleeper = () -> { System.out.println("Zzz"); Thread.sleep(1000); };
+        // Error: Thread.sleep can throw a checked InterruptedException
+
+        Runnable sleeper2 = () -> {
+            System.out.println("Zzz");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        };
+
+        Callable<Void> sleeper3 = () -> {
+            System.out.println("Zzz");
+            Thread.sleep(1000);
+            return null;
+        };
+    }
+}
---
+++ src/main/java/com/demo/ch1/MethodReferences.java
@@ -0,0 +1,21 @@
+package com.demo.ch1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+
+public class MethodReferences extends Application {
+    public void start(Stage stage) {
+        String[] strings = "Mary had a little lamb".split(" ");
+        Arrays.sort(strings, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(strings));
+
+        Button button = new Button("Click me!");
+        button.setOnAction(System.out::println);
+        stage.setScene(new Scene(button));
+        stage.show();
+    }
+}
